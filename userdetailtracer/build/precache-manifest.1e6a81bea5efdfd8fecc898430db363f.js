1d8c4e7b3c746e331d8e1e7a2f73b158562de1a5
Hemant Bhatta <hemub70@gmail.com>
2020-08-03 16:09:54 +0545

    v

---
+++ userdetailtracer/build/precache-manifest.1e6a81bea5efdfd8fecc898430db363f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "243509dbba430a36e9ef1d0a1eb54cd9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75fc2490e1a43e29cadf",
+    "url": "/static/css/main.fb0cd330.chunk.css"
+  },
+  {
+    "revision": "add6634d84c3eee5aa81",
+    "url": "/static/js/2.caa52740.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.caa52740.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75fc2490e1a43e29cadf",
+    "url": "/static/js/main.ecad117c.chunk.js"
+  },
+  {
+    "revision": "41ff227bd1ea1cfd157e",
+    "url": "/static/js/runtime-main.c9ef374d.js"
+  }
+]);
