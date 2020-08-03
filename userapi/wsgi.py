2f9d82430425ad06663b134dc6749483526307d1
Hemant Bhatta <hemub70@gmail.com>
2020-08-03 16:26:02 +0545

    v

--- userapi/wsgi.py
+++ userapi/wsgi.py
@@ -11,5 +11,5 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crudproject1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'userapi.settings')
 application = get_wsgi_application()
