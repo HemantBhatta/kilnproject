090bb4715ab44f44978d9e7aa764dd175dfb3221
Hemant Bhatta <hemub70@gmail.com>
2020-08-11 08:55:04 +0545

    dac

--- userapi/settings.py
+++ userapi/settings.py
@@ -114,24 +114,12 @@ CORS_ALLOW_METHODS = [
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-  
     'default': {
-
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-        'NAME': 'kilndb',
-
-        'USER': 'kilnuser',
-
-        'PASSWORD': 'kilnpw;',
-
-        'HOST': 'localhost',
-
-        'PORT': '5432',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-
 }
+
 
 
 # Password validation
