ae1a1c17ddcac13907de618ee31f5e05843ac7b8
Hemant Bhatta <hemub70@gmail.com>
2020-10-03 12:12:26 +0545

    deploy17

--- userapi/settings.py
+++ userapi/settings.py
@@ -180,5 +180,11 @@ STATICFILES_DIRS = [
 LOGIN_REDIRECT_URL = '/api/workers'
 
 
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+
 
 
